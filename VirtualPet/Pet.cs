22f2640b49a5f340d7f802961d06adfcf615ee3c
Bernard828 <bernard.lackey1@gmail.com>
2020-05-20 16:39:14 -0400

    cleaned up format in pet.cs

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -24,49 +24,57 @@ namespace VirtualPet
             Sleepiness = 80;
 
         }
-       public Pet (string name, string species)
+       public Pet(string name, string species)
         {
             Name = name;
             Species = species;
         }
-        public void SetName( string name)
+
+        public void SetName(string name)
         {
             Name = name;
         }
+
         public string GetName(string name)
         {
             return name;
         }
+
         public void SetSpecies(string species)
         {
             Species = species;
         }
+
         public string GetSpecies( string species)
         {
             return species;
         }
+
         public void SetHunger(int hunger)
         {
             Hunger = hunger;
         }
 
-        public void Feed ()
+        public void Feed()
         {
             Hunger -= 5;
         }
 
-        public int GetHunger ()
+        public int GetHunger()
         {
             return Hunger;
         }
-        public int GetHealth ()
+
+        public int GetHealth()
         {
             return Health;
         }
+
         public void SeeDoctor()
         {
             Health += 30;
         }
+
         public void Tick()
         {
             Hunger += 5;
