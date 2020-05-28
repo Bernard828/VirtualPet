221890430e22f7948b83239456446e06d7b448a5
Bernard828 <bernard.lackey1@gmail.com>
2020-05-27 22:52:07 -0400

    program now has integrated methods for petShelter

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -21,12 +21,12 @@ namespace VirtualPet
                 Console.Clear();
                 Console.WriteLine("\nHello! Welcome to Virtual Pets\n");
                 Console.WriteLine("1. Bringing in a new pet?");
-                Console.WriteLine("2. Feed your pet(s).");
-                Console.WriteLine("3. Give your pet(s) water.");
-                Console.WriteLine("4. Your pet(s) bored, let's play");
-                Console.WriteLine("5. Time for a checkup at with the vet.");
-                Console.WriteLine("6. View list of pets.");
-                Console.WriteLine("6. Adopt a pet!");
+                Console.WriteLine("2. View list of pets.");
+                Console.WriteLine("3. Feed your pet(s).");
+                Console.WriteLine("4. Give your pet(s) water.");
+                Console.WriteLine("5. Your pet(s) bored, let's play.");
+                Console.WriteLine("6. Time for a checkup with the vet.");
+                Console.WriteLine("7. Adopt a pet!");
                 Console.WriteLine("Press enter to return to the Main Menu");
 
                 string userChoice = Console.ReadLine().ToLower();
@@ -50,25 +50,29 @@ namespace VirtualPet
                         newPet.SetSpecies(species);     
                        
                         break;
-
-                    case "2.":
+                    case "2":
+                       newPet.PrintEachPetsStats();
+                        break;
+                    case "3.":
                         Console.WriteLine($"You fed {newPet.GetName()}");
                         newPet.Feed();                    
                         break;
-                    case "3.":
+                    case "4.":
                         Console.WriteLine($"You gave {newPet.GetName()} some water");
                         newPet.GiveWater();
                         break;
-                    case "4.":
+                    case "5.":
                         Console.WriteLine($"{newPet.GetName()} enjoyed the exercise.");
                         newPet.Play();
                         break;
-                    case "5.":
+                    case "6.":
                         Console.WriteLine($" {newPet.GetName()} is feeling much better.");
                         newPet.SeeDoctor();
                         break;
-                    case "6.":
-                      Console.WriteLine($" I know you'll give him a good home!");
+                    case "7.":
+                        Console.WriteLine("Who do you want to take home with you!");
+                        myPets.RemovePet() = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($" I know you'll give him a good home!");
                         break;
                     default:
                         break;
