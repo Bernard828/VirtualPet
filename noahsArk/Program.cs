ac43ea8757cb5ed577aa11ddf1991958d5cd551c
Bernard828 <bernard.lackey1@gmail.com>
2020-09-08 23:09:29 -0400

    created animal class of project

---
+++ noahsArk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace noahsArk
+{
+    class Program
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test line of written code!!!");
+
+            //Animal newAnimal = new Animal();
+            //Man newMan = new Man();
+        }
+    }
+}
