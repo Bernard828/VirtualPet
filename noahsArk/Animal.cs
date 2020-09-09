3da5975bf39639ac7ae63313bedf539ecada5aeb
Bernard828 <bernard.lackey1@gmail.com>
2020-09-08 23:12:28 -0400

    clean code sweep

--- noahsArk/Animal.cs
+++ noahsArk/Animal.cs
@@ -4,7 +4,46 @@ using System.Text;
 
 namespace noahsArk
 {
-    class Animal
+    public class Animal
     {
+        public string Name { get; set; }
+        private string Genus { get; set; }
+        private string Species { get; set; }
+
+        public Animal() { }
+
+        public Animal(string name) { }
+
+        public void SetName(string animalName)
+        {
+            Name = animalName;
+        }
+
+        public void SetGenus (string animalGenus)
+        {
+            Genus = animalGenus;
+        }
+
+        public void SetSpecies( string animalSpecies)
+        {
+            Species = animalSpecies;
+        }
+
+        public virtual string GetName()
+        {
+            return Name;
+        }
+
+        public virtual string GetGenus()
+        {
+            return Genus;
+        }
+
+        public virtual string GetSpecies()
+        {
+            return Species;
+        }
+
+        public virtual void CreateAnimal() { }
     }
 }
